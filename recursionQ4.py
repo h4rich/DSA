56bb49c51eaf6206a7036780ef58c724aac17cc9
harishkumar6375 <harishsuthar739@gmail.com>
2025-06-24 20:18:06 +0530

    DSA problem

---
+++ recursionQ4.py
@@ -0,0 +1,11 @@
+# Find the fibonacci Number using recursion 
+
+def fibonacci(num):
+    if num ==0 or num == 1:
+        return num
+    
+    return fibonacci(num-1)+fibonacci(num-2)
+
+fib = fibonacci(9)
+print(fib)
+
